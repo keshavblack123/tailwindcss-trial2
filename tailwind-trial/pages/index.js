2d9c596b6c5f6d1a5d188bdc2835158ece94b095
keshavblack123 <keshavnatarajan@gmail.com>
2022-09-13 11:58:26 +0800

    initial

---
+++ tailwind-trial/pages/index.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex-col items-center justify-center w-full h-screen Flex">
+      <h1 className='text-xs text-red-700'>Tailwind CSS Trial</h1>
+    </div>
+  )
+}
